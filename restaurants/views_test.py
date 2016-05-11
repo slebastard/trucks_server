c9289f0406274a2e0c5c59c951a40081d563cf93
Sebastien Descahmps <sebastien.deschamps@eleves.enpc.fr>
2016-05-11 16:13:02 +0200

    Tets push sous heroku

--- restaurants/views_test.py
+++ restaurants/views_test.py
@@ -3,6 +3,6 @@ import json
 from django.http import HttpResponse
 
 def show_test(request):
-    test_dict = {"test":"test"}
+    test_dict = {"test":"test","test2":"test2"}
     http_response = HttpResponse(json.dumps(test_dict), content_type='application/json')
     return http_response
