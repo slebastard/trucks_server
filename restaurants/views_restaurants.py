0cec4e5d87ae2ef01eae22f3372c093ebf4c91bd
Arnaud Paran <paran.arnaud@gmail.com>
2016-05-12 15:44:40 +0200

    Corrections on the code and isoformat for the time

--- restaurants/views_restaurants.py
+++ restaurants/views_restaurants.py
@@ -2,6 +2,7 @@ import json
 
 from django.http import HttpResponse
 from django.http import JsonResponse
+from datetime import time
 
 
 def show_resto(request):
@@ -10,18 +11,16 @@ def show_resto(request):
     response_data['resto1']['ID'] = 8
     response_data['resto1']['name'] = 'nom'
     response_data['resto1']['type'] = 'externalKey'
-    response_data['resto1']['disponibility'] =  {}
-    response_data['resto1']['disponibility']['monday'] =
-    response_data['resto1']['disponibility']['monday'] =  ['10h-12h','13h-18h']
-    response_data['resto1']['disponibility']['tuesday'] =  ['10h-22h']
-    response_data['resto1']['disponibility']['wednesday'] =  ['10h-22h']
-    response_data['resto1']['disponibility']['thursday'] =  ['10h-22h']
-    response_data['resto1']['disponibility']['friday'] =  ['10h-22h']
-    response_data['resto1']['disponibility']['saturday'] =  ['10h-22h']
-    response_data['resto1']['disponibility']['sunday'] =  ['10h-22h']
+    response_data['resto1']['disponibility'] = {'monday': [], 'tuesday': [],
+            'wednesday': [], 'thursday': [], 'friday': [], 'saturday': [], 'sunday': []}
+    response_data['resto1']['disponibility']['monday'].append({time(10).isoformat(): time(12).isoformat()})
+    response_data['resto1']['disponibility']['monday'].append({time(13).isoformat(): time(18).isoformat()})
+    response_data['resto1']['disponibility']['tuesday'].append({time(10).isoformat(): time(22).isoformat()})
+    response_data['resto1']['disponibility']['wednesday'].append({time(10).isoformat(): time(22).isoformat()})
+    response_data['resto1']['disponibility']['thursday'].append({time(10).isoformat(): time(22).isoformat()})
+    response_data['resto1']['disponibility']['friday'].append({time(10).isoformat(): time(22).isoformat()})
+    response_data['resto1']['disponibility']['saturday'].append({time(10).isoformat(): time(22).isoformat()})
+    response_data['resto1']['disponibility']['sunday'].append({time(10).isoformat(): time(22).isoformat()})
     response_data['resto1']['tel'] = '+33705432543'
     response_data['resto1']['adress'] = 'adresse'
-    # liste_restos = {ID: 8, name: ' ', type: externalKey, disponibility: {monday: [{IsoStringOpenHour: IsoStringCloseHour}]}, tel: '+33705432543', address: ''}
     return JsonResponse(response_data)
-    # http_response = HttpResponse(json.dumps(response_data), content_type='application/json')
-    # return http_response
