182fe38ba473611c960672549984d61703bf621a
Arnaud Paran <paran.arnaud@gmail.com>
2016-05-22 02:35:40 +0200

    restaurants model created and usable

--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .model_restaurant import Restaurant,OpeningHour,Dish
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'tel', 'address', 'imageUrl')
+
+@admin.register(OpeningHour)
+class OpeningHourAdmin(admin.ModelAdmin):
+    list_display = ('opening', 'closing', 'day')
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'imageUrl', 'description')
--- restaurants/model_restaurant.py
+++ restaurants/model_restaurant.py
@@ -1,117 +1,84 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
 from django.db import models
 
-TYPES_RESTOS = (
-    ('1', "Italien"),
-    ('2', "Japonais"),
-    ('3', "Chinois"),
-    ('4', "Mexicain"),
-    ('5', "Français"),
-    ('6', "Traditionnel"),
-    ('7', "Fast-food"),
-    ('8', "Autre"),
-)
-
-JOUR_SEMAINE = (
-    ('Mon', "Monday"),
-    ('Tue', "Tuesday"),
-    ('Wed', "Wednesday"),
-    ('Thu', "Thursday"),
-    ('Fri', "Friday"),
-    ('Sat', "Saturday"),
-    ('Sun', "Sunday"),
-)
-
-# Classe qui à une horaire d'ouverture associe une horaire de fermeture
-class TrancheHoraire(models.Model):
-
-    horaire_ouverture = models.DateTimeField(
-        verbose_name="Ouverture",
-        null=False, blank=False,
+WEEK_DAYS = (
+        (0, 'monday'),
+        (1, 'tuesday'),
+        (2, 'wednesday'),
+        (3, 'thursday'),
+        (4, 'friday'),
+        (5, 'saturday'),
+        (6, 'sunday')
     )
 
-    horaire_fermeture = models.DateTimeField(
-        verbose_name="Fermeture",
-        null=False, blank=False,
-    )
-
-# Classe qui à une chaine de caractère (jour) associe la liste des TrancheHoraire correspondantes
-class Horaires(models.Model):
-
-    jour_semaine = models.CharField(
-        max_length=300,
-        choices=TYPES_RESTO,
-        verbose_name="jour",
-        )
-
-    ouverture = models.ForeignKey(
-            TrancheHoraire,
-            on_delete=models.CASCADE
-        )
-
-class Restaurants(models.Model):
-
-            'name',
-            'type_resto',
-            'disponibility',
-            'tel',
-            'adress',
-
-    name = models.CharField(
-        max_length=100,
-        verbose_name="Nom",
-    )
-
-    type_resto = models.CharField(
-        max_length=300,
-        choices=TYPES_RESTO,
-        verbose_name="Type",
-    )
-
-    disponibility = models.ForeignKey(
-        'Horaire',
-        verbose_name="Horaires d'ouverture",
-        null=True, blank= True,
-    )
-
-    tel = models.CharField(
-        max_length=100,
-        verbose_name="Telephone",
-    )
-
-    adress = models.CharField(
-        max_length=300,
-        verbose_name="Adresse",
-    )
-
-    # Champs pour traiter facilement les requètes géographiques
-    # location = models.ForeignKey(
-    #     'map.Location',
-    #     verbose_name="Position géographique",
-    #     null=True, blank=True,
-    # )
-
+class OpeningHour(models.Model):
+    opening = models.CharField(max_length=14,
+                            blank=True)
+    closing = models.CharField(max_length=14,
+                            blank=True)
+    day = models.IntegerField(choices=WEEK_DAYS, default=-1)
     def to_dict(self):
-        """ On revoie un JSON qui sera affiché """
-
-        # Fields to be exported
-        fields = [
-            #'ID',
-            'name',
-            'type',
-            'disponibility',
-            'tel',
-            'adress',
-        ]
-
-        # Serialize the values
-        exp_dict = {field: self.serializable_value(field) for field in fields}
-        exp_dict['name'] = self.name
-        exp_dict['type'] = self.type_resto
-        exp_dict['disponibility'] = self.disponibility
-        exp_dict['tel'] = self.tel
-        exp_dict['adress'] = self.adress
-        #exp_dict['f_start_date'] = _date(self.start_date, "F Y")
+        exp_dict = {}
+        exp_dict[self.opening] = self.closing
         return exp_dict
 
+class Dish(models.Model):
+    name = models.CharField(max_length=50,
+                            blank=True)
+    imageUrl = models.CharField(max_length=1000,
+                            blank=True)
+    description = models.CharField(max_length=1000,
+                            blank=True)
+    def to_dict(self):
+        fields = [
+            'id',
+            'name',
+            'imageUrl',
+            'description'
+        ]
+
+        exp_dict = {field: self.serializable_value(field) for field in fields}
+        return exp_dict
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50,
+                            blank=True)
+    address = models.CharField(max_length=100,
+                            blank=True)
+    tel = models.CharField(max_length=12,
+                            blank=True)
+    description = models.CharField(max_length=100,
+                            blank=True)
+    imageUrl = models.CharField(max_length=1000,
+                            blank=True)
+    #TODO change name
+    monday = models.ManyToManyField(OpeningHour)
+    dishes = models.ManyToManyField(Dish)
+
+    def to_dict(self):
+        fields = [
+            'id',
+            'name',
+            'address',
+            'tel',
+            'description',
+            'imageUrl'
+        ]
+
+        exp_dict = {field: self.serializable_value(field) for field in fields}
+        exp_dict['disponibility'] = {
+                'monday': [],
+                'tuesday': [],
+                'wednesday': [],
+                'thursday': [],
+                'friday': [],
+                'saturday': [],
+                'sunday': []
+            }
+        for hour in self.monday.all():
+            exp_dict['disponibility'][WEEK_DAYS[hour.day][1]].append(hour.to_dict())
+
+        exp_dict["dishes"] = [dish.to_dict() for dish in self.dishes.all()]
+        return exp_dict
